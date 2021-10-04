ef6685a533378c91e52f95c61322e41c5a892ce1
emisto <emil1stolpe@gmail.com>
2021-10-04 23:45:58 +0200

    powerup saves horse now

--- src/Engine/ECS/Systems/PowerUpSystem.cpp
+++ src/Engine/ECS/Systems/PowerUpSystem.cpp
@@ -7,8 +7,8 @@
 #include "Rendering.hpp"
 
 PowerUpSystem::PowerUpSystem(ECSManager* ECSManager) 
-	: System(ECSManager, ComponentTypeEnum::POWERUP, ComponentTypeEnum::PLAYER, ComponentTypeEnum::COLLISION),
-	m_powerUpExists(false), m_existingPowerUp(0), m_spawnChance(0.0f), m_powerWeights() {
+	: System(ECSManager, ComponentTypeEnum::POWERUP, ComponentTypeEnum::COLLISION),
+	m_powerUpExists(false), m_spawnChance(0.0f), m_powerWeights() {
 }
 
 void PowerUpSystem::update(float dt) {
@@ -18,62 +18,82 @@ void PowerUpSystem::update(float dt) {
 			spawnPowerUp();
 			m_spawnChance = 0.0f;
 		}
-		m_spawnChance += dt;
 		m_powerUpExists = true;
+		m_spawnChance += dt;
 	}
-
+	
 
 	for (auto& e : m_entities) {
+		PositionComponent* posComp = static_cast<PositionComponent*>(e->getComponent(ComponentTypeEnum::POSITION));
+		float camX = Rendering::getInstance().getCamera()->getPosition().x;
 
-		PowerUpComponent* playerPower = static_cast<PowerUpComponent *>(e->getComponent(ComponentTypeEnum::POWERUP));
-		CollisionComponent* collComp = static_cast<CollisionComponent*>(e->getComponent(ComponentTypeEnum::COLLISION));
+		if (e->hasComponent(ComponentTypeEnum::PLAYER)) {
+			PowerUpComponent* playerPower = static_cast<PowerUpComponent*>(e->getComponent(ComponentTypeEnum::POWERUP));
+			CollisionComponent* collComp = static_cast<CollisionComponent*>(e->getComponent(ComponentTypeEnum::COLLISION));
 
-		//gå igenom alla powerups player kolliderat med
-			//för varje powerup checka vilken typ de är och adda till playerns types
-		for (auto& e2 : collComp->currentCollisionEntities) {
-			PowerUpComponent* collisionPower = static_cast<PowerUpComponent*>(e2->getComponent(ComponentTypeEnum::POWERUP));
-			if (!collisionPower) {
-				//not a powerup!
-				continue;
+			//gå igenom alla powerups player kolliderat med
+				//för varje powerup checka vilken typ de är och adda till playerns types
+			for (auto& e2 : collComp->currentCollisionEntities) {
+				PowerUpComponent* collisionPower = static_cast<PowerUpComponent*>(e2->getComponent(ComponentTypeEnum::POWERUP));
+				if (!collisionPower) {
+					//not a powerup!
+					continue;
+				}
+				//check if powerup power should be added to player powerupcomponent
+				bool powerExists = false;
+				for (int i = 0; i < playerPower->type.size(); i++) {
+					if (playerPower->type[i] == collisionPower->type.front()) {
+						powerExists = true;
+						break;
+					}
+				}
+				//if power does not exist, add it to the player
+				if (!powerExists) {
+					playerPower->type.push_back(collisionPower->type.front());
+					playerPower->activated.push_back(false);
+				}
+
+				//destroy powerup entity and mark that another can spawn
+				m_manager->removeEntity(e2->getID());
+				m_powerUpExists = false;
 			}
-			//check if powerup power should be added to player powerupcomponent
-			bool powerExists = false;
-			for (int i = 0; i < playerPower->type.size(); i++) {
-				if (playerPower->type[i] == collisionPower->type.front()) {
-					powerExists = true;
+
+			for (int i = 0; i < playerPower->type.size() ; i++) {
+				switch (playerPower->type[i]) {
+				case PowerUpType::Hen:
+					//do hen stuff
+					if (playerPower->activated[i]) {
+
+						if (e->hasComponent(ComponentTypeEnum::MOVEMENT) && playerPower->henTimer < 0.0001f) {
+							//save the players life!
+							//add uppwards speed
+							MovementComponent* movComp = static_cast<MovementComponent*>(e->getComponent(ComponentTypeEnum::MOVEMENT));
+							movComp->velocity.y += 30.0f;
+							playerPower->henTimer = 0.0f;
+							//add damagecomponent
+							//start han saving timer
+						}
+						else if(playerPower->henTimer > playerPower->henTime){
+							//if hen saving timer is out remove damagecomponent and hen and activated on same index from players types
+							//reset hen powerup
+							playerPower->type.erase(playerPower->type.begin() + i);
+							playerPower->activated.erase(playerPower->activated.begin() + i);
+						}
+						playerPower->henTimer += dt;
+					}
+
+					
+					break;
+				default:
 					break;
 				}
 			}
-			//if power does not exist, add it to the player
-			if (!powerExists) {
-				playerPower->type.push_back(collisionPower->type.front());
-				playerPower->activated.push_back(false);
-			}
-
-			//destroy powerup entity
-			m_manager->removeEntity(e2->getID());
 		}
-
-		for (int i = 0; i < playerPower->type.size(); i++) {
-			switch(playerPower->type[i]) {
-			case PowerUpType::Hen :
-				//do hen stuff
-				
-				
-				if (e->hasComponent(ComponentTypeEnum::MOVEMENT)) {
-					//save the players life!
-					//add uppwards speed
-					//add damagecomponent
-					//start han saving timer
-				}
-				
-				
-				//if hen saving timer is out remove damagecomponent
-				break;
-			default:
-				break;
-			}
+		else if ((camX - posComp->position.x) > 14.0f) {
+			m_manager->removeEntity(e->getID());
+			m_powerUpExists = false;
 		}
+		
 	}
 
 }
@@ -99,7 +119,7 @@ void PowerUpSystem::spawnPowerUp(){
 	m_manager->addComponent(powerup, graphComp);
 
 	glm::vec2 pos = Rendering::getInstance().getCamera()->getPosition();
-	PositionComponent* posComp = new PositionComponent(pos.x, pos.y+8.0f);
+	PositionComponent* posComp = new PositionComponent(pos.x+14.0f, pos.y+4.0f);
 	m_manager->addComponent(powerup, posComp);
 
 	m_manager->addComponent(powerup, new MovementComponent());
@@ -108,16 +128,3 @@ void PowerUpSystem::spawnPowerUp(){
 	collisionComp->isConstraint = true;
 	m_manager->addComponent(powerup, collisionComp);
 }
-//randoma vilken typ av powerup som ska spawna. Divktade efter hur bra de är
-//vissa kanske bara kan komma upp senare i spelet?
-
-//titta vilka powerups som är upplockade av player. lägg det i playerkomponenten i kollisionssystem kanske?
-//möjligt att mna kan göra så powerups är antingen kontrollerade( tagna) eller att de är fria (kan bli tagna)
-//tagna/fria				finns i palyerkomponent
-//-player kan bara ha en
-//+man kan göra alla powerupgrejer här
-//-fast det kan man inte
-
-//se om de aktiveras
-//för höna titta om playerns position är för låg. ge playern då speed upp och damagekomponent så den förstör tiles
-//för dubbelhopp 
